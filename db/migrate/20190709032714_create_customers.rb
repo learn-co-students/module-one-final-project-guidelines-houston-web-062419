35e6a9860af40f227a01d848703d60455c34b8a0
Aaron Briscoe <gmail2008@gmail.com>
2019-07-08 23:41:24 -0500

    created customer table

--- db/migrate/20190709032714_create_customers.rb
+++ db/migrate/20190709032714_create_customers.rb
@@ -1,16 +1,15 @@
 class CreateCustomers < ActiveRecord::Migration[5.0]
   def change
-
     create_table :customers do |t|
-
       t.string :name
+      t.string :email_address
     end
   end
 end
 
 # rake db:create_migration NAME=create_customers <<<typed this into terminal 
-# and ^this migration file was created. Could not create using rake if a different file had anything in it
-# just stated uninitialized constant Item < I had created the seeds file "Items". 
+# and ^this migration file was created. Could not create using rake if a different file had anything in it.
+# just stated uninitialized constant Item <<< I had created the seeds file "Item". 
 
 
 # rake db:migrate <<<<< written in terminal will add this table bloc ^ created 
@@ -19,3 +18,9 @@ end
 
 # before migrating you need to find out if you need to write a gets.chomp type method in here since 
 # I want the customer to input their information.
+
+#  add column at 39:45 on 1st youtube video
+
+# NEVER REMOVE THINGS FROM SCHEMA FILE!! MIGRATIONS WILL DO THAT FOR YOU
+
+# rake db:rollback -goes back to previous version. 
