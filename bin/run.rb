17caa2e1e6e88f4bd35732c9ec6c4cad1ec9c8c5
Aaron Briscoe <gmail2008@gmail.com>
2019-07-09 18:00:58 -0500

    working on list of items for customers

--- bin/run.rb
+++ bin/run.rb
@@ -4,23 +4,40 @@ class CLI
     def initialize
         @prompt = TTY::Prompt.new 
     end
+    
+    def list_of_items
+    # binding.pry
+        return Item.all.select do |item|
+            item.name
+        end
+    end
+    
+   
 
     def ask_customer_for_item
-        item_name = @prompt.ask("Thanks for creating an account! What would you like to purchase? (Please add item name)")
-        item = Item.find_by(name: item_name)
+        @prompt.ask("Thanks for creating an account! For a list of items sold at our Simple Store press ENTER!")
+
+        list_of_items
+
+        item_name = @prompt.ask("What would you like to purchase? (Please add item name)")
+        item = Item.find_by("lower(name)= ?", item_name.downcase)
         while item == nil
             item_name = @prompt.ask("Please enter a valid item (check spelling or capitalization")
+            
             item = Item.find_by(name: item_name)
         end
         # binding.pry
 
-        return item
+        
     end
 
-    def run_do_stuff_i_dunno
+    def sign_up_and_item_quantity
         @prompt.yes?('Hello and welcome to The Simple Store! Would you like to make a purchases?')
         name = @prompt.ask('Please enter your name:')
-        email = @prompt.ask('Please enter your email address:')
+        email = @prompt.ask('Please enter your email address:') do |q|
+            q.validate(/\A\w+@\w+\.\w+\Z/, 'Invalid email address')
+          end
+        # email = @prompt.ask('Please enter your email address:')
         Customer.create(name: name, email_address: email)
     
         item = ask_customer_for_item
@@ -29,5 +46,7 @@ class CLI
     end
 end
 
-ourCLIInstanceObjectThing = CLI.new
-ourCLIInstanceObjectThing.run_do_stuff_i_dunno
+
+
+cli = CLI.new
+cli.sign_up_and_item_quantity
