3060992ec2655ee75507ef077a6eb3a3aa73190d
abideen11 <abideenkareem17@gmail.com>
2019-07-09 11:28:21 -0500

    readme

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,4 +1,4 @@
-# class Purchase < ActiveRecord::Base 
-#     belongs_to :customer
-#     belongs_to :item
-# end
+class Purchase < ActiveRecord::Base 
+    belongs_to :customer
+    belongs_to :item
+end
