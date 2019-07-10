f20bc9856f84e05d7f4dc8ea793f665a4327785b
abideen11 <abideenkareem17@gmail.com>
2019-07-10 13:44:51 -0500

    readme

--- config/environment.rb
+++ config/environment.rb
@@ -4,10 +4,7 @@ Bundler.require
 require_all 'app'
 
 ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'db/development.db')
-<<<<<<< HEAD
-=======
 ActiveRecord::Base.logger = nil
 
->>>>>>> b9ff0e33d79aaf6be607382b74e012a73203a4c2
 
 
