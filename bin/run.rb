b41eaaf9fa4fc9f701ee69ce9b5b3425e82246e6
jordanbacon <jmbacon92@gmail.com>
2019-07-09 17:19:11 -0700

    working on cli 3

--- bin/run.rb
+++ bin/run.rb
@@ -3,4 +3,7 @@ require_relative '../config/environment'
 welcome
 choice
 student_menu
+house_menu
+value_menu
+# student_info
 # puts "HELLO WORLD"
--- lib/cli.rb
+++ lib/cli.rb
@@ -14,10 +14,11 @@ def welcome
 end
 
 def choice
-    choice = @prompt.select("\nPlease choose from the following options:") do |menu|
-        menu.choice 'Students', 1
-        menu.choice 'Houses', 2
-        menu.choice 'Values', 3
+    puts "\nMain Menu"
+    choice = @prompt.select("Please choose from the following options:") do |menu|
+        menu.choice 'Accio Student Information', 1
+        menu.choice 'Accio House Information', 2
+        menu.choice 'Accio Value Information', 3
         menu.choice 'Exit', 4
     end
 
@@ -32,16 +33,65 @@ def choice
     end
 end
 
-def student_menu(current_user)
-    student_selection = $prompt.select("Please choose a student to see their contact info") do |menu|
-        menu.enum "."
-        users_students(current_user).each_with_index do |student, index|
-            menu.choice student.name, index
-        end
+def student_menu
+    # choice = @prompt.select("\nWould you like to search through names or email addresses?:") do |menu|
+    #     menu.choice 'Names', 1
+    #     menu.choice 'Email Addresses', 2
+    # end
+    puts "\nPlease type the full name of the student you are searching for.\n\n"
+    response = gets.chomp
+# end
+
+# def student_info(response)
+    if Student.name.include?(response)
+        # Student.all.find_by(name: response)
+        puts "Name: #{response}\n"
+    # end
+#         puts "Email Address: #{Student.response.email_address}\n\n"
+    else
+        puts "\nUnfortunately, there is no student matching the name entered.\n"
+#         puts "Please try again or press 'q' to return to the main menu."
     end
-    selected_student = users_students(current_student)[student_selection]
-    student_information(current_user, selected_student)
 end
+
+def house_menu
+    puts "\nPlease type the full name of the house you are searching for.\n\n"
+    response = gets.chomp
+end
+
+def value_menu
+    choice = @prompt.select("Which value do you uphold?:") do |menu|
+        menu.choice 'courage', 1
+        menu.choice 'ambition', 2
+        menu.choice 'loyalty', 3
+        menu.choice 'intelligence', 4
+        menu.choice 'bravery', 5
+        menu.choice 'resourcefulness', 6
+    end
+
+    # if choice == 1
+    #     student_menu()
+    # elsif choice == 2
+    #     house_menu()
+    # elsif choice == 3
+    #     value_menu()
+    # else choice == 4
+    #     return
+    # end
+end
+
+
+
+
+
+    # student_selection = $prompt.select("Please choose a student to see their contact info") do |menu|
+    #     menu.enum "."
+    #     users_students(current_user).each_with_index do |student, index|
+    #         menu.choice student.name, index
+    #     end
+    # end
+    # selected_student = users_students(current_student)[student_selection]
+    # student_information(current_user, selected_student)
 
 
     # item = Item.find_by(name: item_name)
