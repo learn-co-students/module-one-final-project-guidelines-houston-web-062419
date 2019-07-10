31c4baa5ada0f80019e3fac33b714b131b7a9538
jordanbacon <jmbacon92@gmail.com>
2019-07-10 14:47:42 -0700

    working on cli 6

--- lib/cli.rb
+++ lib/cli.rb
@@ -2,8 +2,8 @@
 
 def welcome
 
-    puts "Welcome to Hogwarts!"
-    puts "Please enter your information so we can add you to the Sorting Database."
+    puts "\nWelcome to Hogwarts!\n"
+    puts "\nPlease enter your information so we can add you to the Sorting Database.\n\n"
     name = @prompt.ask('What is your name?', default: ENV['USER'])
     puts name
     email = @prompt.ask('What is your email address? (By entering email address, you agree to receive school communications via email...as well as owl post.)')
@@ -20,6 +20,9 @@ def main_menu
         menu.choice 'Accio House Information', 2
         menu.choice 'Accio Value Information', 3
         menu.choice 'Exit', 4
+
+        menu.choice 'Update Student Information', 5
+        menu.choice 'Report Student', 6
     end
 
     if choice == 1
@@ -28,15 +31,18 @@ def main_menu
         house_menu()
     elsif choice == 3
         value_menu()
-    else choice == 4
+    elsif choice == 4
         exit()
+    elsif choice == 5
+        update_info_menu()
+    else choice == 6
+        report_menu()
     end
 end
 
-def student_menu (response = gets.chomp)
-
+def student_menu
     puts "\nPlease type the full name of the student you are searching for.\n\n"
-    # response = gets.chomp
+    response = gets.chomp
 
     if Student.name.include?(response)
         student_choice = Student.all.find_by(name: response)
@@ -74,9 +80,9 @@ def student_menu (response = gets.chomp)
     end
 end
 
-def house_menu (response = gets.chomp)
+def house_menu
     puts "\nPlease type the name of the house you are searching for.\n\n"
-    # response = gets.chomp
+    response = gets.chomp
 
     if House.name.include?(response)
         house_choice = House.all.find_by(name: response)
@@ -137,4 +143,28 @@ def value_menu
     else choice == 6
         puts "You may like House Slytherin! You also share values with Pansy Parkinson! Go to the Student Search Menu to see how to contact her. "
     end
+end
+
+def exit
+    @prompt.yes?('Are you sure you want to exit?') do |q|
+    end
+end
+
+def update_info_menu
+    puts "Please enter the full name of the student whose email address requires updating."
+    response = gets.chomp
+    user = Student.find_by(name: response)
+    puts "The email address on file is #{user.email_address}"
+    puts "Please enter the new email address."
+    new_response = gets.chomp
+    user.email_address = new_response
+    user.save
+end
+
+def report_menu
+    puts "The proper use of magic is very important to us here at Hogwarts. Please let us know the name of the witch/wizard that you believe has been using magic unethically and we will remove them from our database so that future students are not unintentionally put into contact with them."
+
+    response = gets.chomp
+    user = Student.find_by(name: response)
+    user.destroy
 end
